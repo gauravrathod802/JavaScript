b0c179d8c61f11bd447d5f6d505e00156f53a789
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-12-10 22:24:54 +0530

    updates

--- DOM/Script.js
+++ DOM/Script.js
@@ -1,3 +1,9 @@
 document.write("hello")
 window.alert("hello")
 alert("hii");  //alert is part of window object
+
+console.log(window.document);
+console.dir(window.document);  //to print object we use dir and it is a part of window object
+
+let doc=document.getElementsByClassName("main-div")
+doc.item;
